51f4269f0abe7ff30f5893672258ce46e93e3a1a
luisvelascoserpis <luis.velasco.serpis@gmail.com>
2012-11-30 10:23:42 +0100

    leyendo articulo en MainWindow

--- PArticulo/PArticulo/MainWindow.cs
+++ PArticulo/PArticulo/MainWindow.cs
@@ -9,12 +9,13 @@ using PArticulo;
 
 public partial class MainWindow: Gtk.Window
 {	
+	private IDbConnection dbConnection;
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
 		
 		string connectionString = "Server=localhost;Database=dbprueba;User Id=dbprueba;Password=sistemas";
-		IDbConnection dbConnection = new NpgsqlConnection(connectionString);
+		dbConnection = new NpgsqlConnection(connectionString);
 		dbConnection.Open ();
 		
 		IDbCommand dbCommand = dbConnection.CreateCommand ();
@@ -32,11 +33,12 @@ public partial class MainWindow: Gtk.Window
 		TreeViewExtensions.Fill (treeView, dataReader);
 		dataReader.Close ();
 		
-		dbConnection.Close ();
 	}
 	
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
 	{
+		dbConnection.Close ();
+
 		Application.Quit ();
 		a.RetVal = true;
 	}
@@ -49,12 +51,38 @@ public partial class MainWindow: Gtk.Window
 
 	protected void OnEditActionActivated (object sender, System.EventArgs e)
 	{
+		long id = getSelectedId();
+		
+		Console.WriteLine ("id={0}", id);
+		
+		IDbCommand dbCommand = dbConnection.CreateCommand();
+		//dbCommand.CommandText = "select * from articulo where id="+id;
+		dbCommand.CommandText = string.Format ("select * from articulo where id={0}", id);
+//		dbCommand.CommandText = "select * from articulo where id=:id";
+//		IDbDataParameter dbDataParameter = dbCommand.CreateParameter();
+//		dbCommand.Parameters.Add (dbDataParameter);
+//		dbDataParameter.ParameterName = "id";
+//		dbDataParameter.Value = id;
+		
+		IDataReader dataReader = dbCommand.ExecuteReader ();
+		dataReader.Read ();
+		
+		
 		ArticuloView articuloView = new ArticuloView();
-		articuloView.Nombre = "Introduce el nombre";
-		articuloView.Precio = 1.5;
+		articuloView.Nombre = (string)dataReader["nombre"];
+		articuloView.Precio = double.Parse (dataReader["precio"].ToString ());
 		
 		articuloView.Show ();
 		
+		dataReader.Close ();
 		
+	}
+	
+	private long getSelectedId() {
+		TreeIter treeIter;
+		treeView.Selection.GetSelected(out treeIter);
+		
+		ListStore listStore = (ListStore)treeView.Model;
+		return long.Parse (listStore.GetValue (treeIter, 0).ToString ()); 
 	}
 }
