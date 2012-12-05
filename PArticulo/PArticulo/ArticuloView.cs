ad03a16f19a538c6e3f7fc862d857c7c7c9719ea
luisvelascoserpis <luis.velasco.serpis@gmail.com>
2012-12-05 10:46:14 +0100

    pasada responsabilidad a ArticuloView de PArticulo

--- PArticulo/PArticulo/ArticuloView.cs
+++ PArticulo/PArticulo/ArticuloView.cs
@@ -1,33 +1,49 @@
 using Gtk;
+using Serpis.Ad;
 using System;
+using System.Data;
 
 namespace PArticulo
 {
 	public partial class ArticuloView : Gtk.Window
 	{
-		public ArticuloView () : base(Gtk.WindowType.Toplevel)
+		public ArticuloView (long id) : base(Gtk.WindowType.Toplevel)
 		{
 			this.Build ();
+
+			IDbCommand dbCommand = ApplicationContext.Instance.DbConnection.CreateCommand();
+			dbCommand.CommandText = string.Format ("select * from articulo where id={0}", id);
+			
+			IDataReader dataReader = dbCommand.ExecuteReader ();
+			dataReader.Read ();
+			
+			entryNombre.Text = (string)dataReader["nombre"];
+			spinButtonPrecio.Value = Convert.ToDouble( (decimal)dataReader["precio"] );
+			
+			dataReader.Close ();
+			
+			saveAction.Activated += delegate {
+				Console.WriteLine("saveAction.Activated");
+				
+				IDbCommand dbUpdateCommand = ApplicationContext.Instance.DbConnection.CreateCommand ();
+				dbUpdateCommand.CommandText = "update articulo set nombre=:nombre, precio=:precio where id=:id";
+				
+				AddParameter (dbUpdateCommand, "nombre", entryNombre.Text);
+				AddParameter (dbUpdateCommand, "precio", Convert.ToDecimal (spinButtonPrecio.Value ));
+				AddParameter (dbUpdateCommand, "id", id);
+	
+				dbUpdateCommand.ExecuteNonQuery ();
+				
+				Destroy ();
+			};
 		}
-		
-		public string Nombre { 
-			get {return entryNombre.Text;}
-			set {entryNombre.Text = value;} 
-		}
-		
-		public decimal Precio {
-			get {return Convert.ToDecimal (spinButtonPrecio.Value);}
-			set {spinButtonPrecio.Value = Convert.ToDouble(value);}
-		}
-		
-		public long Categoria {
-			set {
-				//TODO implementar...
-			}
-		}
-		
-		public Gtk.Action SaveAction {
-			get {return saveAction;}
+	
+		public static void AddParameter(IDbCommand dbCommand, string name, object value)
+		{
+			IDbDataParameter dbDataParameter = dbCommand.CreateParameter();
+			dbDataParameter.ParameterName = name;
+			dbDataParameter.Value = value;
+			dbCommand.Parameters.Add (dbDataParameter);
 		}
 	}
 }
